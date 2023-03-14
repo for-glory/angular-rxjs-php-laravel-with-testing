d8f5830ee7327507ba7a2e48fb1d3bb4ae263be0
John Thomas Moye <forglory0905@outlook.com>
2023-03-14 09:10:56 -0700

    Prevent uploading files larger than 50MB on back-end

--- api/app/Http/Controllers/VideoController.php
+++ api/app/Http/Controllers/VideoController.php
@@ -7,6 +7,7 @@ use App\Models\Video;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Validation\Rules\File;
 
 class VideoController extends Controller
 {
@@ -22,6 +23,13 @@ class VideoController extends Controller
 
 	public function saveVideo (Request $req): VideoResource
 	{
+		$req->validate([
+			'video' => [
+				'required',
+				// Size in kilobytes
+				(new File())->max(50 * 1024),
+			],
+		]);
 		$path = Storage::putFile('storage', $req->file('video'));
 		return VideoResource::make(Video::create(['path' => $path]));
 	}
