19ba0644a36cdf2584957a74d2a653ae81c7424c
John Thomas Moye <forglory0905@outlook.com>
2023-03-14 08:47:52 -0700

    Prevent uploading files larger than 50MB on front-end

--- client/src/app/common/components/upload-dialog/upload-dialog.component.ts
+++ client/src/app/common/components/upload-dialog/upload-dialog.component.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { UploadService } from '../../services/upload/upload.service';
 import { UserVideo } from '../../services/upload/user-video';
 
@@ -14,7 +15,8 @@ export class UploadDialogComponent implements OnInit {
 
 	constructor(
 		private uploadService: UploadService,
-		private dialogRef: MatDialogRef<UploadDialogComponent>
+		private dialogRef: MatDialogRef<UploadDialogComponent>,
+		private snackbar: MatSnackBar
 	) {}
 
 	ngOnInit(): void {}
@@ -26,6 +28,13 @@ export class UploadDialogComponent implements OnInit {
 	fileChange(event: Event) {
 		const files: FileList = (event as any).target?.files;
 		if (files.length > 0) {
+			if (files[0].size > 50 * 1024 * 1024) {
+				this.snackbar.open('Uploading files larger than 50MB not allowed', undefined, {
+					duration: 4000,
+				});
+				this.fileInput.nativeElement.value = '';
+				return;
+			}
 			this.file = files[0];
 		}
 	}
