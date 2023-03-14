6846db382122587025e9861afe919b9e91deb89c
John Thomas Moye <forglory0905@outlook.com>
2023-03-14 09:42:01 -0700

    Add show/edit title & desc

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { MatDialog } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { filter, mergeMap, Observable, of, scan, shareReplay, startWith, Subject, tap } from 'rxjs';
 import { UploadDialogComponent } from './common/components/upload-dialog/upload-dialog.component';
+import { ModifyDetailsComponent } from './common/components/modify-details/modify-details.component';
 import { UploadService } from './common/services/upload/upload.service';
 import { UserVideo } from './common/services/upload/user-video';
 
@@ -35,7 +36,18 @@ export class AppComponent implements OnInit {
 			mergeMap((existingVideos: UserVideo[]) => {
 				return this.upload$.pipe(
 					// Scan is a version of reduce() that emits each update
-					scan((videos, newUpload) => videos.concat(newUpload), existingVideos),
+					scan(
+						(videos, newUpload) => {
+							const idx = videos.findIndex((video) => video.id === newUpload.id);
+							videos.splice(
+								idx === -1 ? videos.length : idx,
+								idx === -1 ? 0 : 1,
+								newUpload
+							);
+							return videos;
+						},
+						existingVideos
+					),
 					startWith(existingVideos)
 				);
 			})
@@ -57,6 +69,35 @@ export class AppComponent implements OnInit {
 				// Show success message
 				tap((result: UserVideo | undefined) => {
 					result && this.snackbar.open('Video uploaded successfully', undefined, {
+						duration: 4000
+					});
+				}),
+
+				// Ignore if they close/cancel the dialog
+				filter((result: UserVideo | undefined): result is UserVideo => result != null)
+
+				// No need to unsubscribe because afterClosed emits complete that will clean up this subscription
+			)
+			.subscribe((result: UserVideo) => this.upload$.next(result));
+	}
+
+	modifyDetails(video: UserVideo) {
+		const dialogRef = this.dialog.open<ModifyDetailsComponent, any, UserVideo>(
+			ModifyDetailsComponent, {
+				data: Object.assign({}, video),
+			}
+		);
+		dialogRef
+			.afterClosed()
+			.pipe(
+				// Log out what they did
+				tap((result: UserVideo | undefined) => {
+					console.log('Dialog result:', result ?? 'canceled');
+				}),
+
+				// Show success message
+				tap((result: UserVideo | undefined) => {
+					result && this.snackbar.open('Details modified successfully', undefined, {
 						duration: 4000
 					});
 				}),
